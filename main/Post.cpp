09a6bbc1d653815a01b675b30de41836c12d8668
Aryan Mukerji <aryanmukerji@gmail.com>
2021-12-29 14:46:45 +0530

    UI changes in Post.cpp

--- main/Post.cpp
+++ main/Post.cpp
@@ -154,6 +154,57 @@ long PostList_size = 0;
     {
         return amount_left;
     }
+	
+// ------------------------------- To Make A Table -------------------------------
+template<typename charT, typename traits = std::char_traits<charT> >
+
+class center_helper 
+{
+    std::basic_string<charT, traits> str_;
+    
+    public:
+    
+    center_helper(std::basic_string<charT, traits> str) : str_(str) {}
+    
+    template<typename a, typename b>
+    
+    friend std::basic_ostream<a, b>& operator<<(std::basic_ostream<a, b>& s, const center_helper<a, b>& c);
+};
+
+template<typename charT, typename traits = std::char_traits<charT> >
+
+center_helper<charT, traits> centered1(std::basic_string<charT, traits> str) 
+{
+    return center_helper<charT, traits>(str);
+}
+
+// redeclare for std::string directly so we can support anything that implicitly converts to std::string
+center_helper<std::string::value_type, std::string::traits_type> centered1(const std::string& str) 
+{
+    return center_helper<std::string::value_type, std::string::traits_type>(str);
+}
+
+template<typename charT, typename traits>
+
+std::basic_ostream<charT, traits>& operator<<(std::basic_ostream<charT, traits>& s, const center_helper<charT, traits>& c) 
+{
+    std::streamsize w = s.width();
+    
+    if (w > c.str_.length()) 
+    {
+        std::streamsize left = (w + c.str_.length()) / 2;
+        s.width(left);
+        s << c.str_;
+        s.width(w - left);
+        s << "";
+    } 
+    else 
+    {
+        s << c.str_;
+    }
+    
+    return s;
+}
 
 void display_PostList()
 {
@@ -163,13 +214,44 @@ void display_PostList()
         return ;
     }
 	
+	cout << "\n\n";
+	cout << "+" << std::setw(40) << centered1(" -------------------------------------- ")
+		 << "+" << std::setw(40) << centered1(" -------------------------------------- ")
+		 << "+" << std::setw(40) << centered1(" -------------------------------------- ") 
+		 << "+" << std::setw(40) << centered1(" -------------------------------------- ") 
+		 << "+" << std::endl;
+	
+	cout << "|" << std::setw(40) << centered1(" Post ID ")
+		 << "|" << std::setw(40) << centered1(" Post Name ")
+         << "|" << std::setw(40) << centered1(" Amount Required ")
+		 << "|" << std::setw(40) << centered1(" Amount Left ")
+		 << "|" << std::endl;
+							   
+	cout << "+" << std::setw(40) << centered1(" -------------------------------------- ")
+		 << "+" << std::setw(40) << centered1(" -------------------------------------- ")
+		 << "+" << std::setw(40) << centered1(" -------------------------------------- ")
+		 << "+" << std::setw(40) << centered1(" -------------------------------------- ")
+		 << "+" << std::endl;
+	
     for(int i = 0; i < PostList_size; i++)
     {
-        cout << "\n " << PostList[i].get_Post_ID() 
+		cout << "|" << std::setw(40) << centered1(to_string(PostList[i].get_Post_ID()))
+		 << "|" << std::setw(40) << centered1(PostList[i].get_Post_Title())
+         << "|" << std::setw(40) << centered1(to_string(PostList[i].get_amount_requested()))
+		 << "|" << std::setw(40) << centered1(to_string(PostList[i].get_amount_left()))
+		 << "|" << std::endl;
+		 
+        /* cout << "\n " << PostList[i].get_Post_ID() 
 			 << " : " << PostList[i].get_Post_Title() 
              << " | " << PostList[i].get_amount_requested() 
-			 << " | " << PostList[i].get_amount_left();
+			 << " | " << PostList[i].get_amount_left(); */
     }
+	
+	cout << "+" << std::setw(40) << centered1(" -------------------------------------- ")
+		 << "+" << std::setw(40) << centered1(" -------------------------------------- ")
+		 << "+" << std::setw(40) << centered1(" -------------------------------------- ")
+		 << "+" << std::setw(40) << centered1(" -------------------------------------- ")
+		 << "+" << std::endl;
 }
 
 void display_Post(long Post_ID)
